f2a56b5f0b0664551fe60915a46f6ca2307bf307
sardorv <sardor@3i.ai>
2020-12-26 11:29:12 +0900

    valid mountain problem solution added

--- validmountainarray.py
+++ validmountainarray.py
@@ -1,6 +1,8 @@
 def isValidMountainArray(arr):
     i = 1
     size = len(arr)
+    if size < 3:
+        return False
     while i < size and arr[i] > arr[i - 1]:
         i += 1
     if i == 1 or i == size:
